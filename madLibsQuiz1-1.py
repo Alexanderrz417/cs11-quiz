a80b2ce3eae35973bf93372950a381e4cadf9446
Alexanderrz417 <2938898@Stocktonusd.org>
2025-11-30 06:11:34 +0000

    fixed what i saw wrong couple syntax errors and what not, some punctiation too

--- madLibsQuiz1-1.py
+++ madLibsQuiz1-1.py
@@ -4,25 +4,23 @@
 
 print("************************************")
 print("|                                  |")
-print("|      Welcome to MadLibs          | ") 
+print("|      Welcome to MadLibs          |") 
 print("|                                  |")
-print(************************************)
+print("************************************")
 
-(''MadLibs is a fill-in-the-blanks story
-game. The player must choose words based on the 
-given prompts, and the computer will return a 
-short story that includes the words the user chose.''')
+print("MadLibs is a fill-in-the-blanks story game.")
+print("The player must choose words based on the given prompts,")
+print("and the computer will return a short story that includes the words the user chose.")
 
-play = onput("Do you want to play MadLibs?(y/n) ")
-
-if play = "y"
+play = input("Do you want to play MadLibs?(y/n) ")
+if play == "y":
     person_name = input("Choose a name for a person: ")
     place = input("Choose a place: ")
     noun_1 = input("Choose a singlular noun: ")
-    animal_1 = input(Choose an animal: )
+    animal_1 = input("Choose an animal: ")
     adjective_1 = input("Choose an adjective for a feeling: ")
     adjective_2 = input("Choose an adjective: ")
-    adjective_3  input("Choose an adjective: ")
+    adjective_3 = input("Choose an adjective: ")
     animal_2 = input("Choose an animal: ")
     food = input("Choose a food: ")
     
@@ -34,7 +32,7 @@ if play = "y"
     hiking on the """ +adjective_2+""" trail. If I see a """ +adjective_3 +""" """ +animal_2+"""
     on the hike, I will take it home as my new pet! The best part of 
     camping is eating """ +food+""" by the campfire!
-    """)
+    "")
     
     print("Thanks for playing! Goodbye!")
     
