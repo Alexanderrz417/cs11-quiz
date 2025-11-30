9b14c9f636a9da42f98e46781f9aba943dd95ad5
Alexanderrz417 <2938898@Stocktonusd.org>
2025-11-30 06:25:47 +0000

    made a lil thingy that solves basic addition and subtraction problems

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+def add_numbers(a, b):
+    return a + b 
 
+def subtract_numbers(a, b):
+    return a - b
+
+choice = input("Do you want to do addition or subtraction? (add/subtract): ").lower()
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if choice == "add":
+    result = add_numbers(num1, num2)
+    print("The result of addition is:", result)
+elif choice == "subtract":
+    result = subtract_numbers(num1, num2)
+    print("The result of subtraction is:", result)
+else:
+    print("Invalid choice. Please type 'add' or 'subtract'.")
